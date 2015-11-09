6aab23f686135ef3f7d36fda96e12977478c06fe
Praakrit Pradhan <pradhapt@mail.uc.edu>
2015-11-08 23:42:28 -0500

    Just added a little prime checker.
    Kind of not working, but might work, not sure.

--- project3.m
+++ project3.m
@@ -10,3 +10,5 @@ plain_txt = input('Enter message to send: \n','s');
 num_plain_txt = double(plain_txt)
 bin_plain_txt = dec2bin(num_plain_txt)
 
+x = input('Enter primenumber');
+out =  primecheck(x);
