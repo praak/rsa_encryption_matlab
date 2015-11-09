621ea0f28335649d1c9e2cf163647a1a3f4f4f92
Praakrit Pradhan <pradhapt@mail.uc.edu>
2015-11-08 20:03:45 -0500

    Project3_inMATLAB
    Just trying a few things in MATLAB.

---
+++ project3.m
@@ -0,0 +1,12 @@
+% -------------------------------------------------
+% Praakrit Pradhan
+% Charles, Kaushal, Lizz
+% Advanced data structures and algorithms
+% Project/Homework 3
+% -------------------------------------------------
+
+plain_txt = input('Enter message to send: \n','s');
+
+num_plain_txt = double(plain_txt)
+bin_plain_txt = dec2bin(num_plain_txt)
+
